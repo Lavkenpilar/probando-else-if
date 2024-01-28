0f6606a9e28eb7dc049270ef4d40c8d3caff27f9
Lavken <lavkenpilar@gmail.com>
2024-01-28 18:37:07 -0300

    create index-assets-js

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+
+const button = document.querySelector ('button')
+console.log (button)
+button.addEventListener ("click", function(){
+const sexo = document.querySelector ('#input').value
+    console.log (input)
+if (false) {
+    console.log ("Este mensaje no se mostrará")
+} else if (true) {
+    console.log ("Este mensaje tampoco")
+} else if (true) {
+    console.log ("Este mensaje si se mostrará")
+} else {
+    console.log ("A mi no me verás")
+}
+})
